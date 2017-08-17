cdcf23c229b067c182a3b5c1b4a6450f0e570ce2
Marcelo Henrique da Silva de Bairros <MARCELOHS180@asav.org.br>
2017-08-17 16:17:47 -0300

    Document resolveTemplate function iss#2

--- src/js/vmodal.js
+++ src/js/vmodal.js
@@ -1,3 +1,11 @@
+/**
+ * Find which template is the choosen between a set of templates.
+ *
+ * @export
+ * @param {Object[]} templates contains type and builder properties
+ * @param {String} type the choosen property
+ * @returns {Function} the builder property from the choosen template
+ */
 export function resolveTemplate(templates, type) {
   return templates.reduce((previousTemplate, currentTemplate) => {
     if (currentTemplate.type === type) {
