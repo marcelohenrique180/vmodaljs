2df48132d01da05b4ed0ef06edc964596eeba5b9
Marcelo Henrique da Silva de Bairros <MARCELOHS180@asav.org.br>
2017-08-18 15:11:38 -0300

    Modularize resolveTemplate function iss#6

--- src/js/vmodal.js
+++ src/js/vmodal.js
@@ -3,23 +3,7 @@ import {
   vmodalErrorTemplate,
   vmodalSuccessTemplate
 } from '../templates';
-
-/**
- * Find which template is the choosen between a set of templates.
- *
- * @export
- * @param {Object[]} templates contains type and builder properties
- * @param {String} type the choosen property
- * @returns {Function} the builder property from the choosen template
- */
-export function resolveTemplate(templates, type) {
-  return templates.reduce((previousTemplate, currentTemplate) => {
-    if (currentTemplate.type === type) {
-      return currentTemplate.builder;
-    }
-    return previousTemplate;
-  }, templates[0].builder);
-}
+import resolveTemplate from './resolvers/template';
 
 export default function vmodal({ title, message, type }) {
   const noScroll = 'body--no-scroll';
